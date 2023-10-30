6719abaf4d93ff6153b9bfbcf9711a02c25ad896
mayank <mpandey2196@gmail.com>
2023-10-30 18:20:01 +0530

    new files added

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 const apiKey="d1498d826983553616152a4ab3e4bae0";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const weatherIcon=document.querySelector('.weather-img');
+let weatherCard=document.querySelector('.weather');
+
 
 function getData(){
 
     const city = document.querySelector('.search').children[0].value;
     checkWeather(city);
+    weatherCard.classList.toggle('active');
     
 
 }
@@ -15,6 +18,16 @@ function getData(){
 async function checkWeather(city){
     
     const response =await fetch(apiUrl + city +`&appid=${apiKey}`);
+    
+    if(response.status==404){
+        document.querySelector('.error').style.display="block";
+        document.querySelector('.weather').style.display="none";
+    }
+    else{
+        document.querySelector('.error').style.display="none";
+        document.querySelector('.weather').style.display="block";
+    }
+
     var data=await response.json();
     console.log(data);
     document.querySelector('#city').innerHTML=data.name;
@@ -23,19 +36,19 @@ async function checkWeather(city){
     document.querySelector('#windspeed').innerHTML=data.wind.speed + " km/h";
 
     if(data.weather[0].main=="Clouds"){
-        weatherIcon.src="/weatherapp/images/weather images/cloudy.png";
+        weatherIcon.src="images/weather images/cloudy.png";
     }
     if(data.weather[0].main=="Clear"){
-        weatherIcon.src="/weatherapp/images/weather images/sunny.png";
+        weatherIcon.src="/images/weather images/sunny.png";
     }
     if(data.weather[0].main=="Rain"){
-        weatherIcon.src="/weatherapp/images/weather images/rainy.png";
+        weatherIcon.src="/images/weather images/rainy.png";
     }
     if(data.weather[0].main=="Mist"){
-        weatherIcon.src="/weatherapp/images/weather images/cloudy.png";
+        weatherIcon.src="/images/weather images/cloudy.png";
     }
     if(data.weather[0].main=="Drizzle"){
-        weatherIcon.src="/weatherapp/images/weather images/rainy.png";
+        weatherIcon.src="/images/weather images/rainy.png";
     }
 /* cloudy
 rainy
